fd4f9b8b763f1390fac28258793b6ec230d7b6b1
Didi2009 <100088766+Didi2009@users.noreply.github.com>
2023-12-09 19:31:56 +0000

    Add files via upload

---
+++ invisiblity_cloak.py
@@ -0,0 +1,30 @@
+import cv2
+import time
+import numpy as np
+fourcc=cv2.VideoWriter_fourcc(*"XVID")
+output_file=cv2.VideoWriter("output.avi",fourcc,20,0,(640,480))
+cap=cv2.VideoCapture(0)
+time.sleep(2)
+bg=0
+for i in range(60):
+    ret,bg=cap.read()
+bg=np.flip(bg,axis=1)
+
+while(cap.isOpened()):
+    ret,img=cap.read
+    if not ret:
+        break
+    img=np.flip(img,axis=1)
+    hsv=cv2.cvtColour(img,cv2.COLOR_BGR2HSV)
+    lower_red=np.array([0,120,50])
+    upper_red=np.array([10,255,255])
+    mask_1=cv2.inRange([hsv,lower_red,upper_red])
+
+    lower_red=np.array([170,120,70])
+    upper_red=np.array([180,255,255])
+    mask_2=cv2.inRange([hsv,lower_red,upper_red])
+    cv2.imshow("mask_1",mask_1)
+    cv2.waitKey(1)
+cap.release()
+#out.release()
+cv2.destroyAllWindows()
